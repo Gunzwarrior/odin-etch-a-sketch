2aaa9c1f56f7eae444942db60eb09e0b3bb77a0a
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-15 12:36:58 +0100

    Add a reset button

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let gridSize = 16;
 let numberOfSquares = gridSize**2;
 console.log(numberOfSquares);
 const container = document.querySelector("#container");
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetGrid);
 
+function resetGrid() {
+  const gridReset = document.querySelectorAll(".gridSquareColored");
+  gridReset.forEach((gridSquare) => {
+    gridSquare.classList.remove("gridSquareColored");
+  });
+};
 
 function colorSquare(e) {
   e.target.classList.add("gridSquareColored");
