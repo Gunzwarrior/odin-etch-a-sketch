d7794a49c1625e42af160c98b3dc05ea719c6500
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-15 12:22:18 +0100

    Create the hover effect

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ console.log(numberOfSquares);
 const container = document.querySelector("#container");
 
 
+function colorSquare(e) {
+  e.target.classList.add("gridSquareColored");
+}
+
 function generateGrid(number) {
   for (i = 0; i < number; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("gridSquare");
+    newDiv.addEventListener("mouseenter", colorSquare)
     container.appendChild(newDiv);
   }
 }
