cda743a1f48263ba0a87a0ce825708ecd08c2a3f
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-15 20:12:20 +0100

    Create a clunky fade-in version

--- script.js
+++ script.js
@@ -23,12 +23,37 @@ function resetGrid() {
   generateGrid(numberOfSquares);
 };
 
+let currentStyle;
 function colorSquare(e) {
-  // e.target.classList.add("gridSquareColored");
-  let rValue = Math.floor(Math.random()*256);
-  let gValue = Math.floor(Math.random()*256);
-  let bValue = Math.floor(Math.random()*256);
-  e.target.style.backgroundColor = `rgb(${rValue}, ${gValue}, ${bValue})`;
+  // e.target.classList.add("gridSquareColored"); -Standard mode
+  
+  // let rValue = Math.floor(Math.random()*256); -Rainbow mode
+  // let gValue = Math.floor(Math.random()*256);
+  // let bValue = Math.floor(Math.random()*256);
+  // e.target.style.backgroundColor = `rgb(${rValue}, ${gValue}, ${bValue})`;
+  
+  currentStyle = e.target.style.cssText;
+  if (currentStyle.includes("rgb(230, 230, 230)")) {
+    e.target.style.backgroundColor = "rgb(204, 204, 204)";
+  } else if (currentStyle.includes("rgb(204, 204, 204)")) {
+    e.target.style.backgroundColor = "rgb(179, 179, 179)";
+  } else if (currentStyle.includes("rgb(179, 179, 179)")) {
+    e.target.style.backgroundColor = "rgb(153, 153, 153)";
+  } else if (currentStyle.includes("rgb(153, 153, 153)")) {
+    e.target.style.backgroundColor = "rgb(128, 128, 128)";
+  } else if (currentStyle.includes("rgb(128, 128, 128)")) {
+    e.target.style.backgroundColor = "rgb(102, 102, 102)";
+  } else if (currentStyle.includes("rgb(102, 102, 102)")) {
+    e.target.style.backgroundColor = "rgb(77, 77, 77)";
+  } else if (currentStyle.includes("rgb(77, 77, 77)")) {
+    e.target.style.backgroundColor = "rgb(51, 51, 51)";
+  } else if (currentStyle.includes("rgb(51, 51, 51)")) {
+    e.target.style.backgroundColor = "rgb(26, 26, 26)";
+  } else if (currentStyle.includes("rgb(26, 26, 26)")) {
+    e.target.style.backgroundColor = "rgb(0, 0, 0)";
+  } else {
+    e.target.style.backgroundColor = "rgb(230, 230, 230)";
+  }
 }
 
 function generateGrid(number) {
